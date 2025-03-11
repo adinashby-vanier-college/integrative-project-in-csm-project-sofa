5917d02f563e430867e5aea2fe5566108f43c636
Obaidah <2376893@edu.vaniercollege.qc.ca>
2025-03-10 21:38:52 -0400

    Added libraries for everyone

--- Deliverable 3 - Part 1/GravityAndOrbits/src/main/java/com/example/gravityandorbits/HelloApplication.java
+++ Deliverable 3 - Part 1/GravityAndOrbits/src/main/java/com/example/gravityandorbits/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.gravityandorbits;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +12,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Label label = new Label("Welcome to JavaFX.");
+        label.setScaleX(2);
+        label.setScaleY(2);
+        StackPane sp = new StackPane(label);
+        Scene scene = new Scene(/*fxmlLoader.load()*/ sp, 500, 400);
+        stage.setTitle("Welcome to JavaFX!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
